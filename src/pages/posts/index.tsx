27b8c331f2da39c3e9a79e64ada785af7f69cea5
Stefano Bacchini <stebacchini@gmail.com>
2022-01-19 19:36:09 -0300

    Criar páginas de post e validação de assinatura

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,23 @@
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client'
+import * as prismicH from '@prismicio/helpers'
 import styles from './styles.module.scss'
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -10,23 +26,47 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de janeiro de 2022</time>
-            <strong>Titulo</strong>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem corrupti, laborum temporibus eligendi praesentium, nostrum fugiat esse doloremque quod sequi deserunt expedita quaerat, ratione dignissimos alias? Vero expedita possimus enim?</p>
-          </a>
-          <a href="#">
-            <time>12 de janeiro de 2022</time>
-            <strong>Titulo</strong>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi architecto deserunt fuga ratione asperiores quae facere soluta blanditiis consequatur iusto dicta dolores consequuntur sapiente possimus minima at vero, commodi rerum.</p>
-          </a>
-          <a href="#">
-            <time>12 de janeiro de 2022</time>
-            <strong>Titulo</strong>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam libero nihil ad fugiat dignissimos quia? Optio sed est a quis aliquid asperiores dignissimos minima tempora illo. Dignissimos maxime distinctio minima!</p>
-          </a>
+          { posts.map(post => (
+            <Link href={`/posts/${post.slug}`}>
+            <a key={post.slug} >
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+            </Link>
+          ))}
+
         </div>
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+  })
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: prismicH.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  });
+  return {
+    props: {
+      posts
+    }
+  }
 }
